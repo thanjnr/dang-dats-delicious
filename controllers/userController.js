e412c7f22b228ab59baed9c000b8f2d5ea00a76a
thanjnr <ethomas.han@gmail.com>
2017-06-06 22:48:33 +1200

    CLEANUP: removed console output

--- controllers/userController.js
+++ controllers/userController.js
@@ -11,8 +11,6 @@ exports.registerForm = (req, res) => {
 }
 
 exports.validateRegister = (req, res, next) => {
-
-    console.log(req.body)
     req.sanitizeBody('name');
     req.checkBody('name', 'You must supply a name!').notEmpty();
     req.checkBody('email', 'That email is not valid!').isEmail();
@@ -30,7 +28,6 @@ exports.validateRegister = (req, res, next) => {
         req.flash('error', errors.map(err => err.msg));
         res.render('register', { title: 'Register', body: req.body, flashes: req.flash()});
     }
-    console.log(req.body)
     next();
 }
 
