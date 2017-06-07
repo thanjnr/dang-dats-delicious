31d458c7a911a5ae76b25bd574f92c99706072e1
thanjnr <ethomas.han@gmail.com>
2017-06-07 23:49:57 +1200

    FEATURE: hearts imp...

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
+const User = mongoose.model('User');
 const multer = require('multer');
 const jimp = require('jimp');
 const uuid = require('uuid');
@@ -126,4 +127,14 @@ exports.mapPage = (req, res) => {
     console.log('rendering map...');
     res.render('map', { title: 'Map'});
 };
+
+exports.heartStore = async (req, res) => {
+    const hearts = req.user.hearts.map(obj => obj.toString());
+    const operator = hearts.includes(req.params.id) ? '$pull' : '$addToSet';
+    const user = await User.findByIdAndUpdate(req.user._id, 
+        { [operator]: { hearts: req.params.id } },
+        { new: true }
+    );
+    res.json(user);
+}
 
